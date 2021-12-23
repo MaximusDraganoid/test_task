56ffb8ed48b9b4c0abd535138d4ed976b32f5cc0
Maksim Maslov <maslowmaxim@gmail.com>
2021-12-23 10:59:22 +0300

    init commit

---
+++ src/test/java/ru/maslov/simple/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maslov.simple.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
